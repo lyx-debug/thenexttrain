195f3ca0ce2778c288c105d77389fdb901f684e9
lyx-debug <lxlyx6212@outlook.com>
2025-04-14 09:56:13 +0800

    123098

--- js/stationdata-beijing.js
+++ js/stationdata-beijing.js
@@ -4,10 +4,8 @@ const metroLines = [
         color: "#034ba0",
         stations: [
             { name: "草桥", isTransferStation: false },
-            { name: "笆斗山", isTransferStation: false },
-            { name: "燕子矶", isTransferStation: false },
-            { name: "吉祥庵", isTransferStation: false },
-            { name: "晓庄", isTransferStation: true }
+            { name: "大兴新城", isTransferStation: false },
+            { name: "大兴机场", isTransferStation: false }
             
         ]
     },
@@ -15,36 +13,8 @@ const metroLines = [
         name: "2",
         color: "#A6093D",
         stations: [
-            { name: "鱼嘴", isTransferStation: false },
-            { name: "天保街", isTransferStation: false },
-            { name: "青莲街", isTransferStation: false },
-            { name: "螺塘路", isTransferStation: true },
-            { name: "油坊桥", isTransferStation: true },
-            { name: "雨润大街", isTransferStation: false },
-            { name: "元通", isTransferStation: true },
-            { name: "奥体东", isTransferStation: false },
-            { name: "兴隆大街", isTransferStation: false },
-            { name: "集庆门大街", isTransferStation: false },
-            { name: "云锦路", isTransferStation: false },
-            { name: "莫愁湖", isTransferStation: false },
-            { name: "汉中门", isTransferStation: false },
-            { name: "上海路", isTransferStation: false },
-            { name: "新街口", isTransferStation: true },
-            { name: "大行宫", isTransferStation: true },
-            { name: "西安门", isTransferStation: false },
-            { name: "明故宫", isTransferStation: false },
-            { name: "苜蓿园", isTransferStation: false },
-            { name: "下马坊", isTransferStation: false },
-            { name: "孝陵卫", isTransferStation: false },
-            { name: "钟灵街", isTransferStation: false },
-            { name: "马群", isTransferStation: true },
-            { name: "金马路", isTransferStation: true },
-            { name: "仙鹤门", isTransferStation: false },
-            { name: "学则路", isTransferStation: false },
-            { name: "仙林中心", isTransferStation: false },
-            { name: "羊山公园", isTransferStation: false },
-            { name: "南大仙林校区", isTransferStation: false },
-            { name: "经天路", isTransferStation: false }
+            
+            
         ]
     }
     
